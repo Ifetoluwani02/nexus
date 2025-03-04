35d7b936ca8fc39cf65e24a2d3cd2c34c1b69225
ladibanks1 <oladipupobankole24@gmail.com>
2025-03-04 11:10:49 +0100

    added navigation link to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import "../styles/home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="home">
+      {/* The Navigation Link */}
+      <div>
+        <section className="flex justify-evenly p-5">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/login"}>Link</Link>
+          <Link to={"/register"}>Register</Link>
+          <Link to={"/transactions"}>Transactions</Link>
+          <Link to={"/wallet"}>Wallet</Link>
+          <Link to={"/chat"}>Chat</Link>
+          <Link to={"/admin"}>Admin</Link>
+        </section>
+      </div>
       <h2>Welcome to Nexus Financial App</h2>
       <p>Manage your finances with ease and security.</p>
     </div>
